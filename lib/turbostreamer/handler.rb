210ddb587ef983f24cde169e4d283e6940d98552
Anton <anton.estum@gmail.com>
2022-10-31 19:10:00 +0300

    Option 3

--- lib/turbostreamer/handler.rb
+++ lib/turbostreamer/handler.rb
@@ -4,23 +4,22 @@ require "active_support/core_ext"
 
 class TurboStreamer
   class Handler
-    
+
     class_attribute :default_format
     self.default_format = :json
-    
+
     def self.supports_streaming?
       true
     end
-    
+
     # TODO: setting source=nil is for rails 5.x compatability, once unsppored
     # source can be a required param and
     # `source = template.source if source.nil?` can be removed
-    def self.call(template, source=nil)
-      source = template.source if source.nil?
+    def self.call(template, source)
       # this juggling is required to keep line numbers right in the error
       %{__already_defined = defined?(json); json||=TurboStreamer::Template.new(self, output_buffer: output_buffer || ActionView::OutputBuffer.new); #{source}
         json.target! unless (__already_defined && __already_defined != "method")}
     end
-    
+
   end
 end
